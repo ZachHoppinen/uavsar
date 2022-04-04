bca7977bbb2cbb9aafdcab73310f885da7b37c06
ZachKeskinen <zachkeskinen@gmail.com>
2022-04-03 18:56:59 -0600

    generalized from bare earth to all aso

--- src/lidar/merge_tiles.py
+++ src/lidar/merge_tiles.py
@@ -1,8 +1,8 @@
 """
-Downloads uavsar images from the Alaska Satellite Facility's Vertex service. The script downloads interferogram, amplitude, coherence, unwrapped phase, and incidence angle of uavsar imgs from the vertex website and converts them to .tiff files.
+Tiles ASO Lidar tiles into a complete image.
 
 Usage:
-    download-uavsar.py [-c csv_fp] [-o out_dir] [-d debug]
+    merge_tiles.py [-i in_dir] [-o out_fp] [-p projected]
 
 Options:
     -i in_dir     Directory containing folders with .adf files to merge
@@ -12,27 +12,29 @@ Options:
 
 from glob import glob
 import os
-from os.path import join,exists,basename,dirname
+from os.path import join, exists, basename, dirname
 import rasterio
 from rasterio.merge import merge
 import subprocess
 import shlex
 from docopt import docopt
 
-def merge(args):
-    out_fp = args.get('-o')
-    in_dir = args.get(-'i')
-    tiff_dir = join(in_dir, 'tiff')
+def merge_tiles(args):
+    output_fp = args.get('-o')
+    in_dir = args.get('-i')
+    tiff_dir = join(in_dir, 'tiffs')
     os.makedirs(tiff_dir, exist_ok = True)
 
     wgs84 = args.get('-p')
 
-    l = sorted(glob(join(lidar_dir, 'be_bsmt_*/')))
+    l = sorted(glob(join(in_dir, '*/')))
+    l = [f for f in l if len(glob(join(f, '*.adf'))) > 0]
     for d in l:
         in_fp = join(d, 'w001001.adf')
         out_fp = join(tiff_dir, basename(d[:-1])+'.tiff')
-        cmd = shlex.split(f'gdal_translate -of GTiff {in_fp} {out_fp}')
-        subprocess.run(cmd, stdout= subprocess.PIPE)
+        if not exists(out_fp):
+            cmd = shlex.split(f'gdal_translate -of GTiff {in_fp} {out_fp}')
+            subprocess.run(cmd, stdout= subprocess.PIPE)
 
     src_files_to_mosaic = []
     for tiff in glob(join(tiff_dir, '*.tiff')):
@@ -49,13 +51,14 @@ def merge(args):
         }
     )
 
-    with rasterio.open(out_fp, 'w', **output_meta) as m:
+    with rasterio.open(output_fp, 'w', **output_meta) as m:
         m.write(mosaic)
+
     if wgs84:
         from rasterio.warp import calculate_default_transform, reproject, Resampling
 
         dst_crs = 'EPSG:4326'
-        with rasterio.open(out_fp) as src:
+        with rasterio.open(output_fp) as src:
             transform, width, height = calculate_default_transform(
                 src.crs, dst_crs, src.width, src.height, *src.bounds)
             kwargs = src.meta.copy()
@@ -66,7 +69,7 @@ def merge(args):
                 'height': height
             })
 
-            with rasterio.open(out_fp.replace('.tiff','_wgs84.tiff'), 'w', **kwargs) as dst:
+            with rasterio.open(output_fp.replace('.tiff','_wgs84.tiff'), 'w', **kwargs) as dst:
                 for i in range(1, src.count + 1):
                     reproject(
                         source=rasterio.band(src, i),
@@ -80,4 +83,4 @@ def merge(args):
 
 if __name__ == '__main__':
     args = docopt(__doc__)
-    merge(args)
+    merge_tiles(args)
