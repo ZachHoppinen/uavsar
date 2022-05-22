a911dc05d4b4a3f9aadf317fd06afdd24cd75c5f
zacharykeskinen <zacharykeskinen@borah-login.cm.cluster>
2022-05-22 15:28:26 -0600

    multiprocessing first round success

--- src/funcs/ulmo_extract.py
+++ src/funcs/ulmo_extract.py
@@ -26,16 +26,12 @@ today = datetime.today().strftime('%Y-%m-%d')
 
 def raster_box_extract(img, x_coord, y_coord, box_side = 5):
     meta = img.meta
-
     # Use the transform in the metadata and your coordinates
     rowcol = rio.transform.rowcol(meta['transform'], xs=x_coord, ys=y_coord)
-
     w = img.read(1, window=Window(rowcol[1], rowcol[0], box_side, box_side))
-
     return w
 
 def snotel_fetch(sitecode, variablecode='SNOTEL:SNWD_D', start_date='1950-10-01', end_date=today):
-    #print(sitecode, variablecode, start_date, end_date)
     values_df = None
     try:
         #Request data from the server
@@ -73,12 +69,11 @@ def get_snotel_image_results(img_fp, inc_fp, ann_fp, snotel_gdf = sites_gdf_conu
             e = pd.to_datetime(ann.loc[0, 'start time of acquisition for pass 2'])
             values = snotel_fetch(r['code'], variablecode=variable, start_date=s, end_date=e)
             # Get units
-            unit = ulmo.cuahsi.wof.get_variable_info(wsdlurl, variable)['units']['name']
-            d['sds'] = imperial_to_metric(values['value'], unit)
-            d['phase'] = raster_box_extract(src, r.geometry.x, r.geometry.y, bbox_side)
-            d['inc'] = raster_box_extract(inc_src, r.geometry.x, r.geometry.y, bbox_side)
-            res[r['code']] = d
-
-    return res
+            if values is not None:
+                unit = ulmo.cuahsi.wof.get_variable_info(wsdlurl, variable)['units']['name']
+                d['sds'] = imperial_to_metric(values['value'], unit)
+                d['phase'] = raster_box_extract(src, r.geometry.x, r.geometry.y, bbox_side)
+                d['inc'] = raster_box_extract(inc_src, r.geometry.x, r.geometry.y, bbox_side)
+                res[r['code']] = d
 
     return res
--- src/uavsar_snotel_sd/uavsar_snotel_sd_ulmo.py
+++ src/uavsar_snotel_sd/uavsar_snotel_sd_ulmo.py
@@ -3,46 +3,51 @@ from os.path import join, exists, basename, dirname, expanduser
 from glob import glob
 import numpy as np
 import pandas as pd
-import rasterio as rio
-from tqdm import tqdm, tqdm_notebook
-import matplotlib.pyplot as plt
 import pickle
 from ulmo_extract import get_snotel_image_results
-import multiprocessing
+from multiprocessing import Pool, cpu_count
 from datetime import datetime
 
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
 print('Starting snotel sd vs uavsar sd analysis...')
+print(f'Creating {cpu_count()}-process pool')
 res = pd.DataFrame()
 
 with open(expanduser('~/scratch/data/uavsar/image_fps'), 'rb') as f:
     image_fps = pickle.load(f)
 
 image_fps = [f for f in image_fps if f['fp'].endswith('.unw.grd.tiff')]
+image_fps = iter(image_fps)
 
-def process(img, res):
+tmp_dir = expanduser(f'~/uavsar/results/uavsar_snotel_sd/tmp/')
+
+def process(img):
     dic = {}
     desc = pd.read_csv(img['ann'], index_col = 0)
+    dic['name'] = basename(img['fp'])
     dic['first_dt'] = pd.to_datetime(desc.loc['value', 'start time of acquisition for pass 1'])
     dic['second_dt'] = pd.to_datetime(desc.loc['value', 'start time of acquisition for pass 2'])
     dic['pol'] = basename(img['fp']).split('_')[6][4:]
     dic['result'] = get_snotel_image_results(img['fp'], img['inc'], img['ann'])
-    res = pd.concat([res, pd.DataFrame.from_records([dic])])
-    with open(expanduser(f'~/uavsar/results/uavsar_snotel_sd/res_df'), 'wb') as f:
-        pickle.dump(res, f)
-    return res
+    # res = pd.concat([res, pd.DataFrame.from_records([dic])])
+    with open(join(tmp_dir, basename(img['fp'])), 'wb') as f:
+        pickle.dump(dic, f)
 
-loc_old = None
+# loc_old = None
 start_time = datetime.now()
-for img in image_fps:
-    loc_new = img['location']
-    if loc_new != loc_old:
-        print(f'Starting {loc_new}')
-    res = process(img, res)
-    print(res.shape)
-    loc_old = loc_new
+
+os.makedirs(tmp_dir, exist_ok= True)
+
+pool = Pool()                         # Create a multiprocessing Pool
+pool.map(process, image_fps)
+
+res = pd.DataFrame()
+for f in glob(join(tmp_dir, '*')):
+    with open(f, 'rb') as f:
+        dic = pickle.load(f)
+    res = pd.concat([res, pd.DataFrame.from_records([dic])])
 
 res['diff_dt'] = (res['second_dt'] - res['first_dt']).astype('timedelta64[D]')
 with open(expanduser(f'~/uavsar/results/uavsar_snotel_sd/res_df'), 'wb') as f:
