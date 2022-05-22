647e6401aaa302cdb0b9377507dfb47da772f6a4
zacharykeskinen <zacharykeskinen@borah-login.cm.cluster>
2022-05-22 13:57:24 -0600

    working on multiprocessing

--- src/uavsar_snotel_sd/uavsar_snotel_sd_ulmo.py
+++ src/uavsar_snotel_sd/uavsar_snotel_sd_ulmo.py
@@ -9,8 +9,10 @@ import matplotlib.pyplot as plt
 import pickle
 from ulmo_extract import get_snotel_image_results
 import multiprocessing
+from datetime import datetime
 
-data_dir = '/bsuscratch/zacharykeskinen/data/uavsar/images'
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
 
 print('Starting snotel sd vs uavsar sd analysis...')
 res = pd.DataFrame()
@@ -20,7 +22,7 @@ with open(expanduser('~/scratch/data/uavsar/image_fps'), 'rb') as f:
 
 image_fps = [f for f in image_fps if f['fp'].endswith('.unw.grd.tiff')]
 
-def process(img):
+def process(img, res):
     dic = {}
     desc = pd.read_csv(img['ann'], index_col = 0)
     dic['first_dt'] = pd.to_datetime(desc.loc['value', 'start time of acquisition for pass 1'])
@@ -30,10 +32,21 @@ def process(img):
     res = pd.concat([res, pd.DataFrame.from_records([dic])])
     with open(expanduser(f'~/uavsar/results/uavsar_snotel_sd/res_df'), 'wb') as f:
         pickle.dump(res, f)
+    return res
 
+loc_old = None
+start_time = datetime.now()
 for img in image_fps:
-    process(img)
+    loc_new = img['location']
+    if loc_new != loc_old:
+        print(f'Starting {loc_new}')
+    res = process(img, res)
+    print(res.shape)
+    loc_old = loc_new
 
 res['diff_dt'] = (res['second_dt'] - res['first_dt']).astype('timedelta64[D]')
 with open(expanduser(f'~/uavsar/results/uavsar_snotel_sd/res_df'), 'wb') as f:
     pickle.dump(res, f)
+
+end_time = datetime.now()
+print(f'Run Time: {end_time - start_time}')
