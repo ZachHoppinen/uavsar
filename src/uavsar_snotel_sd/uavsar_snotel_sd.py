160fa57ba6c69f70e07aeaa6dc797721609e8b56
zacharykeskinen <zacharykeskinen@borah-login.cm.cluster>
2022-05-30 19:59:37 -0600

    incorporate calfiornia snotel

--- src/uavsar_snotel_sd/uavsar_snotel_sd.py
+++ src/uavsar_snotel_sd/uavsar_snotel_sd.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 import pickle
 from ulmo_extract import get_snotel_image_results
+from metloom_extract import get_california_snowsites
 from gee_ancillary import snow_off_phase, atmospheric_h20_diff
 from multiprocessing import Pool, cpu_count
 from datetime import datetime
@@ -31,8 +32,9 @@ def process(img):
     dic['first_dt'] = pd.to_datetime(desc.loc['value', 'start time of acquisition for pass 1'])
     dic['second_dt'] = pd.to_datetime(desc.loc['value', 'start time of acquisition for pass 2'])
     dic['pol'] = basename(img['fp']).split('_')[6][4:]
-    # img_fp, inc_fp, cor_fp, ann_fp
-    dic['ulmo_result'] = get_snotel_image_results(img_fp = img['fp'], inc_fp = img['inc'], cor_fp = img['cor'], ann_fp = img['ann'], box_side = 50)
+    dic['snotel_result'] = get_snotel_image_results(img_fp = img['fp'], inc_fp = img['inc'], cor_fp = img['cor'], ann_fp = img['ann'], box_side = 50)
+    if ', CA' in img['fp']:
+        dic['cdec_result'] = get_california_snowsites(img_fp = img['fp'], inc_fp = img['inc'], cor_fp = img['cor'], ann_fp = img['ann'], box_side = 50)
     dic['snow_off_phase'] = snow_off_phase(img_fp = img['fp'], ann_fp = img['ann'])
     dic['h20_atmospheric_diff'] = atmospheric_h20_diff(img_fp = img['fp'], ann_fp = img['ann'])
     with open(join(tmp_dir, basename(img['fp'])), 'wb') as f:
@@ -56,7 +58,7 @@ for f in glob(join(tmp_dir, '*')):
     res = pd.concat([res, pd.DataFrame.from_records([dic])])
 
 res['diff_dt'] = (res['second_dt'] - res['first_dt']).astype('timedelta64[D]')
-with open(expanduser(f'~/uavsar/results/uavsar_snotel_sd/res_df_v3'), 'wb') as f:
+with open(expanduser(f'~/uavsar/results/uavsar_snotel_sd/res_df_v4'), 'wb') as f:
     pickle.dump(res, f)
 
 end_time = datetime.now()
